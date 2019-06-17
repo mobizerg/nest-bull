a59b126eab99a58d9bb1504db1f612ab8e3e9d28
Frozenex <arun.frozenex@gmail.com>
2019-06-17 17:30:08 +0530

    Removed lodash

--- lib/bull.helper.ts
+++ lib/bull.helper.ts
@@ -1,5 +1,3 @@
-import { upperCase } from 'lodash';
-
 export function getQueueToken(name?: string): string {
-  return name ? `BULL_QUEUE_${upperCase(name)}` : 'BULL_QUEUE_DEFAULT';
+  return name ? `BULL_QUEUE_${name.toUpperCase()}` : 'BULL_QUEUE_DEFAULT';
 }
